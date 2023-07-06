f9d96882dfb04f62e2a7731a401d07af2629429f
biancamandrade <biancamdandrade@gmail.com>
2023-07-06 19:28:58 -0300

    Switch case e if else

---
+++ switch-case-js/switch-case.js
@@ -0,0 +1,108 @@
+const month = 10
+switch(month) {
+    case 1:
+
+        console.log("O mês é Janeiro");
+    break;
+
+    case 2:
+        
+        console.log("O mês é Fevereiro");
+    break;
+
+    case 3:
+        
+        console.log("O mês é Março");
+    break;
+
+    case 4:
+      
+        console.log("O mês é Abril");
+    break;
+
+    case 5:
+        
+        console.log("O mês é Maio");
+    break;
+
+    case 6:
+     
+        console.log("O mês é Junho");
+    break;
+
+    case 7:
+        
+        console.log("O mês é Julho");
+    break;
+
+    case 8:
+        
+        console.log("O mês é Agosto");
+    break;
+
+    case 9:
+        
+        console.log("O mês é Setembro");
+    break;
+
+    case 10:
+      
+        console.log("O mês é Outubro");
+    break;
+
+    case 11:
+        
+        console.log("O mês é Novembro");
+    break;
+
+    case 12:
+    
+        console.log("O mês é Dezembro");
+    break;
+    
+
+
+    default:
+        console.log("O mês não existe.")
+        }
+
+
+const letra = 'e';
+    if (letra === 'a' || letra === 'e' || letra === 'i'|| letra === 'o' || letra === 'u') {
+        console.log("A letra é uma vogal")
+    }
+    else {
+        console.log("A letra é uma consoante")
+    }
+
+const nota = "5"
+    if(nota === "1") {
+        console.log("O resultado foi muito ruim")
+    }
+    else if(nota === "2") {
+        console.log("O resultado foi ruim")
+    }
+    else if(nota === "3") {
+        console.log("O resultado foi regular")
+    }
+    else if(nota === "4") {
+        console.log("O resultado foi bom")
+    }
+    else if(nota === "5") {
+        console.log("O resultado foi muito bom")
+    }
+    else {
+            console.log("O resultado nao existe")
+    }
+
+
+const notaa = "60"
+    if(notaa >= 60) {
+            console.log("Você foi aprovado")
+    }
+    else{
+        console.log("Você foi reprovado")
+    }
+
+
+
